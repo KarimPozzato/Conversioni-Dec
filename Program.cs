5ef26276432ce6a760c2ea9d744cb568991ba419
KarimPozzato <94352240+KarimPozzato@users.noreply.github.com>
2023-10-05 19:04:47 +0200

    Version 2 Program.cs

--- Program.cs
+++ Program.cs
@@ -4,42 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Converti_Dec_Bin_2
+namespace Convert_Bin_Dec_3
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Inserisci un indirizzo IP decimale puntato (es. 10.10.10.10): ");
+            Console.Write("Inserisci un indirizzo IP decimale puntato (es. 10.10.10.10): ");
             string input = Console.ReadLine();
 
-            if (ValidaInput(input))
+            if (ValidaInput(input, out int[] decimalePuntato))
             {
-                int[] decimalePuntato = ParsaInput(input);
+                bool[] risultatoBinario = Convet_DP_To_Binario(decimalePuntato);
 
-                bool[] risultatoBinario = Converti_DP_To_Binario(decimalePuntato);
+                Console.WriteLine("Risultato in binario:");
+                StampaArrayBool(risultatoBinario);
 
-                Console.WriteLine($"Risultato binario: {risultatoBinario}");
-                //StampaArrayBool(risultatoBinario);
-                
+                int risultatoIntero = Convert_DP_To_Intero(decimalePuntato);
+                Console.WriteLine($"Da intero a decimale puntato: {risultatoIntero}");
+
+                int risultatoInteroDaBinario = Convert_Binario_To_Intero(risultatoBinario);
+                Console.WriteLine($"Da intero a binario: {risultatoInteroDaBinario}");
+
+                int[] risultatoDecimalePuntatoDaBinario = Convert_Binario_To_DP(risultatoBinario);
+                Console.WriteLine($"Da decimale puntato a binario: {risultatoDecimalePuntatoDaBinario}");
+            }
+            else
+            {
+                Console.WriteLine("Input non valido.");
+                Console.WriteLine("Inserire un indirizzo IP decimale puntato valido.");
             }
 
             Console.ReadLine();
         }
 
-        static bool ValidaInput(string input)
+        static bool ValidaInput(string input, out int[] decimalePuntato)
         {
-            // Verifica se l'input ha il formato corretto (X.X.X.X)
             string[] parti = input.Split('.');
+            decimalePuntato = new int[4];
+
             if (parti.Length != 4)
             {
                 return false;
             }
 
-            // Verifica se ciascuna parte è un numero valido
-            foreach (string parte in parti)
+            for (int i = 0; i < 4; i++)
             {
-                if (!int.TryParse(parte, out int numero) || numero < 0 || numero > 255)
+                if (!int.TryParse(parti[i], out decimalePuntato[i]) || decimalePuntato[i] < 0 || decimalePuntato[i] > 255)
                 {
                     return false;
                 }
@@ -48,36 +59,73 @@ namespace Converti_Dec_Bin_2
             return true;
         }
 
-        static int[] ParsaInput(string input)
-        {
-            string[] parti = input.Split('.');
-            int[] decimalePuntato = new int[4];
-
-            for (int i = 0; i < 4; i++)
-            {
-                decimalePuntato[i] = int.Parse(parti[i]);
-            }
-
-            return decimalePuntato;
-        }
-
-        static bool[] Converti_DP_To_Binario(int[] decimalePuntato)
+        static bool[] Convet_DP_To_Binario(int[] decimalePuntato)
         {
             bool[] risultatoBinario = new bool[32];
             int indiceBinario = 0;
 
             foreach (int numero in decimalePuntato)
             {
-                string binario = Convert.ToString(numero, 2).PadLeft(8, '0');
-
-                for (int i = 0; i < 8; i++)
+                for (int j = 7; j >= 0; j--)
                 {
-                    risultatoBinario[indiceBinario] = binario[i] == '1';
+                    risultatoBinario[indiceBinario] = (numero & (1 << j)) != 0;
                     indiceBinario++;
                 }
             }
 
             return risultatoBinario;
         }
-    }
+
+        static int Convert_DP_To_Intero(int[] decimalePuntato)
+        {
+            int risultatoIntero = 0;
+
+            foreach (int numero in decimalePuntato)
+            {
+                risultatoIntero = (risultatoIntero << 8) | numero;
+            }
+
+            return risultatoIntero;
+        }
+
+        static int Convert_Binario_To_Intero(bool[] binario)
+        {
+            int risultatoIntero = 0;
+
+            foreach (bool bit in binario)
+            {
+                risultatoIntero = (risultatoIntero << 1) | (bit ? 1 : 0);
+            }
+
+            return risultatoIntero;
+        }
+
+        static int[] Convert_Binario_To_DP(bool[] binario)
+        {
+            int[] risultatoDecimalePuntato = new int[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                int valore = 0;
+
+                for (int j = 0; j < 8; j++)
+                {
+                    valore = (valore << 1) | (binario[i * 8 + j] ? 1 : 0);
+                }
+
+                risultatoDecimalePuntato[i] = valore;
+            }
+
+            return risultatoDecimalePuntato;
+        }
+
+        static void StampaArrayBool(bool[] array)
+        {
+            foreach (bool valore in array)
+            {
+                Console.Write(valore ? "1" : "0");
+            }
+            Console.WriteLine();
+        }
+    }    
 }
